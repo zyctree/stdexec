f478e37e38c66f3c715e63b5e2bd11cb28f1a428
Bryce Adelstein Lelbach aka wash <brycelelbach@gmail.com>
2022-10-07 11:46:15 -0700

    * Enable transparent unified memory when compiling with NVC++. * Use `std::vector` instead of device_vector` in the reduction example.

--- examples/cuda/examples/reduce.cpp
+++ examples/cuda/examples/reduce.cpp
@@ -1,8 +1,3 @@
-#include <range/v3/range/concepts.hpp>
-#include <range/v3/view/subrange.hpp>
-
-#include <thrust/device_vector.h>
-
 #include <execution.hpp>
 #include <schedulers/stream.cuh>
 
@@ -10,14 +5,11 @@ namespace ex = std::execution;
 namespace stream = example::cuda::stream;
 
 int main() {
-  const int n = 2 * 1024;
-  thrust::device_vector<int> input(n, 1);
-  auto r = ranges::subrange(thrust::raw_pointer_cast(input.data()),
-                            thrust::raw_pointer_cast(input.data()) + input.size());
+  std::vector<int> input(2048, 1);
 
   stream::context_t stream_context{};
 
-  auto snd = ex::transfer_just(stream_context.get_scheduler(), r)
+  auto snd = ex::transfer_just(stream_context.get_scheduler(), input)
            | stream::reduce();
 
   auto [result] = std::this_thread::sync_wait(std::move(snd)).value();
